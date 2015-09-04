80fe0ca969117342243a6699a95011bcc4daa21e
Marcos Ortiz <marcosluis2186@gmail.com>
2015-09-04 08:28:27 -0400

    Added Ticket model

---
+++ lib/hotel_beds/model/ticket.rb
@@ -0,0 +1,28 @@
+require "hotel_beds/model"
+require "hotel_beds/model/available_room"
+require "hotel_beds/model/contract"
+require "hotel_beds/model/destination"
+require "hotel_beds/parser/room_grouper"
+
+module HotelBeds
+  module Model
+    class Ticket
+      include HotelBeds::Model
+
+      # attributes
+      attribute :code, String
+      attribute :availability_token, String
+      attribute :name, String
+      attribute :images, Array[String]
+      attribute :longitude, BigDecimal
+      attribute :latitude, BigDecimal
+      attribute :available_rooms, Array[HotelBeds::Model::AvailableRoom]
+      attribute :contract, HotelBeds::Model::Contract
+      attribute :destination, HotelBeds::Model::Destination
+
+      def grouped_rooms(requested_rooms)
+        HotelBeds::Parser::RoomGrouper.new(requested_rooms, available_rooms).groups
+      end
+    end
+  end
+end
