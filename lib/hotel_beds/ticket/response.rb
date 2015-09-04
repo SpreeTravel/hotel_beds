c978d9f37e70284879857f13ac488494a16f88ec
Marcos Ortiz <marcosluis2186@gmail.com>
2015-09-04 08:28:00 -0400

    Added ticket response

---
+++ lib/hotel_beds/ticket/response.rb
@@ -0,0 +1,43 @@
+require "hotel_beds/action/response"
+require "hotel_beds/parser/ticket"
+
+module HotelBeds
+  module Ticket
+    class Response < HotelBeds::Action::Response
+      def session_id
+        request.session_id
+      end
+
+      def current_page
+        if data = pagination_data
+          Integer(data.attr("currentPage"))
+        else
+          0
+        end
+      end
+
+      def total_pages
+        if data = pagination_data
+          Integer(data.attr("totalPages"))
+        else
+          0
+        end
+      end
+
+      def currency
+        body.at_css("Currency").attr("code")
+      end
+      ## Agregar lo referente a tickets
+      def hotels
+        body.css("ServiceHotel").lazy.map do |hotel|
+          HotelBeds::Parser::Hotel.new(hotel).to_model
+        end
+      end
+
+      private
+      def pagination_data
+        body.at_css("PaginationData")
+      end
+    end
+  end
+end
