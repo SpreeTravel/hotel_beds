961bae9df4a0a338beaf859aa99d8b74887732fb
Marcos Ortiz <marcosluis2186@gmail.com>
2015-09-04 08:28:54 -0400

    Added Available Modality parser

---
+++ lib/hotel_beds/parser/available_modality.rb
@@ -0,0 +1,34 @@
+require "hotel_beds/parser"
+require "hotel_beds/parser/price"
+require "hotel_beds/parser/contract"
+
+module HotelBeds
+  module Parser
+    class AvailableModality
+      include HotelBeds::Parser
+
+      # attributes
+      attribute :id, selector: "HotelRoom", attr: "SHRUI"
+      attribute :room_count, selector: "HotelOccupancy RoomCount"
+      attribute :adult_count, selector: "HotelOccupancy AdultCount"
+      attribute :child_count, selector: "HotelOccupancy ChildCount"
+      attribute :number_available, selector: "HotelRoom", attr: "availCount"
+      attribute :description, selector: "HotelRoom RoomType"
+      attribute :board, selector: "HotelRoom Board"
+      attribute :board_code, selector: "HotelRoom Board", attr: "code"
+      attribute :room_type_code, selector: "HotelRoom RoomType", attr: "code"
+      attribute :room_type_characteristic,
+        selector: "HotelRoom RoomType", attr: "characteristic"
+      attribute :price, selector: "HotelRoom > Price > Amount"
+      attribute :cancellation_policies,
+        selector: "HotelRoom CancellationPolicy", multiple: true,
+        parser: HotelBeds::Parser::CancellationPolicy
+      attribute :rates,
+        selector: "Price PriceList Price", multiple: true,
+        parser: HotelBeds::Parser::Price
+      attribute :customers,
+        selector: "HotelOccupancy GuestList Customer", multiple: true,
+        parser: HotelBeds::Parser::Customer
+    end
+  end
+end
