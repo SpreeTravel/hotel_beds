efae910ba9bdd4f74c1e3e1ad492a49e32f4271f
Marcos Ortiz <marcosluis2186@gmail.com>
2015-09-04 08:27:12 -0400

    Added ticket operation

---
+++ lib/hotel_beds/ticket/operation.rb
@@ -0,0 +1,16 @@
+require "hotel_beds/action/operation"
+require_relative "envelope"
+require_relative "request"
+require_relative "response"
+
+module HotelBeds
+  module Ticket
+    class Operation < HotelBeds::Action::Operation
+      remote_method :getTicketAvail
+      remote_namespace :TicketAvailRQ
+      envelope_class Envelope
+      request_class Request
+      response_class Response
+    end
+  end
+end
