34a7151a0f4b1474334895f6c5af302b1caca83a
Marcos Ortiz <marcosluis2186@gmail.com>
2015-09-17 14:08:18 -0400

    Added maximum_waiting_time and vehicle_type to Transfer parser

--- lib/hotel_beds/parser/transfer.rb
+++ lib/hotel_beds/parser/transfer.rb
@@ -2,22 +2,32 @@ require "hotel_beds/parser"
 require "hotel_beds/parser/contract"
 require "hotel_beds/parser/destination"
 require "hotel_beds/parser/available_modality"
+require "hotel_beds/parser/transferbullet_point"
 
 module HotelBeds
   module Parser
-    class Ticket
+    class Transfer
       include HotelBeds::Parser
 
       # attributes
-      attribute :code, selector: "TicketInfo > Code"
+      attribute :code, selector: "TransferInfo > Code"
       attribute :availability_token, attr: "availToken"
-      attribute :name, selector: "TicketInfo > Name"
-      attribute :description, selector: "TicketInfo > DescriptionList > Description"
-      attribute :images, selector: "TicketInfo > ImageList > Image > Url",
+      attribute :transfer_type, attr: "transferType"
+      attribute :date_from, attr: "DateFrom"
+      attribute :total_amount, attr: "TotalAmount"
+      attribute :net_price, attr: "NetPrice"
+      attribute :descriptions, selector: "TransferInfo > DescriptionList > Description",
         multiple: true
-      attribute :ticket_zone, attr: "TicketInfo > TicketZone"
+      attribute :images, selector: "TransferInfo > ImageList > Image > Url",
+        multiple: true
+      attribute :type, attr: "TransferInfo > Type"
+      attribute :vehicle_type, attr: "TransferInfo > VehicleType"
       attribute :company_code, attr: "TicketInfo > CompanyCode"
-      attribute :ticket_class, attr: "TicketInfo > TicketClass"
+      attribute :maximum_waiting_time, attr: "TransferInfo > TransferSpecificContent > MaximumWaitingTime"
+      attribute :contract, selector: "ContractList > Contract",
+                parser: HotelBeds::Parser::Contract
+      attribute :transferbullet_point, selector: "TransferInfo > TransferSpecificContent > GenericTransferGuidelinesList > TransferBulletPoint",
+                parser: HotelBeds::Parser::TransferBulletPoint
       attribute :destination, selector: "TicketInfo > Destination",
         parser: HotelBeds::Parser::Destination
       attribute :available_modality, selector: "AvailableModality", multiple: true,
