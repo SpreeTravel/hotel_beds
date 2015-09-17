1ab0649937516254bcaccd47cc7b39cff0f4b812
Marcos Ortiz <marcosluis2186@gmail.com>
2015-09-17 11:28:55 -0400

    Added Transfer parser

---
+++ lib/hotel_beds/parser/transfer.rb
@@ -0,0 +1,27 @@
+require "hotel_beds/parser"
+require "hotel_beds/parser/contract"
+require "hotel_beds/parser/destination"
+require "hotel_beds/parser/available_modality"
+
+module HotelBeds
+  module Parser
+    class Ticket
+      include HotelBeds::Parser
+
+      # attributes
+      attribute :code, selector: "TicketInfo > Code"
+      attribute :availability_token, attr: "availToken"
+      attribute :name, selector: "TicketInfo > Name"
+      attribute :description, selector: "TicketInfo > DescriptionList > Description"
+      attribute :images, selector: "TicketInfo > ImageList > Image > Url",
+        multiple: true
+      attribute :ticket_zone, attr: "TicketInfo > TicketZone"
+      attribute :company_code, attr: "TicketInfo > CompanyCode"
+      attribute :ticket_class, attr: "TicketInfo > TicketClass"
+      attribute :destination, selector: "TicketInfo > Destination",
+        parser: HotelBeds::Parser::Destination
+      attribute :available_modality, selector: "AvailableModality", multiple: true,
+        parser: HotelBeds::Parser::AvailableModality
+    end
+  end
+end
