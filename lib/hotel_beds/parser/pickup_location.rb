76a62276e1487972b8a607772be86f51f18101fa
Marcos Ortiz <marcosluis2186@gmail.com>
2015-09-17 14:16:10 -0400

    Added PickupLocation parser

---
+++ lib/hotel_beds/parser/pickup_location.rb
@@ -0,0 +1,15 @@
+require "hotel_beds/parser"
+
+module HotelBeds
+  module Parser
+    class TransferBulletPoint
+      include HotelBeds::Parser
+
+      # attributes
+      attribute :code, attr: "PickupLocation > Code"
+      attribute :name, selector: "PickupLocation > Name"
+      attribute :transferzone_code, selector: "PickupLocation > TransferZone > Code"
+      attribute :terminal_type, selector: "PickupLocation > TerminalType"
+    end
+  end
+end
