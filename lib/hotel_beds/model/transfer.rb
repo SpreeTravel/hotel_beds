e7265568bd58e7b42a0ca3e93d8207e2f76b7d9e
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2015-11-11 11:46:35 -0500

    transfer info fetching finished

--- lib/hotel_beds/model/transfer.rb
+++ lib/hotel_beds/model/transfer.rb
@@ -7,22 +7,45 @@ module HotelBeds
       include HotelBeds::Model
 
       # attributes
-      attribute :code, String
       attribute :availability_token, String
+      attribute :transfer_type, String
+
+      attribute :contract, HotelBeds::Model::Contract
+
       attribute :date_from, Date
-      attribute :currency, Date
+      attribute :time_from, Integer
+      attribute :currency, String
       attribute :total_amount, BigDecimal
       attribute :net_price, BigDecimal
+
+      attribute :code, String
+      attribute :type, String
+      attribute :type_code, String
+      attribute :vehicle_type, String
+      attribute :vehicle_type_code, String
       attribute :description, String
       attribute :images, Array[String]
-      attribute :product_specification, Array[String]
-      attribute :ticket_zone, String
-      attribute :type, String
+      attribute :max_waiting, Integer
+
+      attribute :adult, Integer
+      attribute :child, Integer
+
+      # attribute :pick_location, String
+      attribute :pick_location_code, String
+      attribute :pick_location_type, String
+      attribute :dest_location_code, String
+      attribute :dest_location_type, String
+
+      attribute :duration, Integer
+
       attribute :vehicle_type, String
       attribute :product_type, String
       attribute :service_type, String
-      attribute :contract, HotelBeds::Model::Contract
-      attribute :transferbullet_point, Array[HotelBeds::Model::TransferBulletPoint]
+      attribute :product_specification, Array[String]
+
+
+      # attribute :ticket_zone, String
+      # attribute :transferbullet_point, Array[HotelBeds::Model::TransferBulletPoint]
 
     end
   end
--- lib/hotel_beds/parser/transfer.rb
+++ lib/hotel_beds/parser/transfer.rb
@@ -1,6 +1,7 @@
 require "hotel_beds/parser"
 require "hotel_beds/parser/contract"
 require "hotel_beds/parser/destination"
+require "hotel_beds/parser/pickup_location"
 
 module HotelBeds
   module Parser
@@ -10,22 +11,40 @@ module HotelBeds
       # attributes
       attribute :availability_token, attr: "availToken"
       attribute :transfer_type, attr: "transferType"
-      attribute :date_from, selector: "DateFrom"
+
+      attribute :contract, selector: "ContractList > Contract",
+                parser: HotelBeds::Parser::Contract
+
+      attribute :date_from, selector: "DateFrom", attr: "date"
+      attribute :time_from, selector: "DateFrom", attr: "time"
       attribute :currency, selector: "Currency", attr: "code"
       attribute :total_amount, selector: "TotalAmount"
-      attribute :duration, selector: "EstimatedTransferDuration"
       attribute :net_price, selector: "NetPrice"
+
+      attribute :code, selector: "TransferInfo > Code"
+      attribute :type, selector: "TransferInfo > Type"
+      attribute :type_code, selector: "TransferInfo > Type", attr: "code"
+      attribute :vehicle_type, selector: "TransferInfo > VehicleType"
+      attribute :vehicle_type_code, selector: "TransferInfo > VehicleType", attr: "code"
+      attribute :description, selector: "TransferInfo > DescriptionList > Description", multiple: true
+      attribute :images, selector: "TransferInfo > ImageList > Image > Url", multiple: true
+      attribute :max_waiting, selector: "TransferInfo > TransferSpecificContent > MaximumWaitingTime", attr: "time"
+
+      attribute :adult, selector: "Paxes > AdultCount"
+      attribute :child, selector: "Paxes > ChildCount"
+
+      attribute :pick_location_code, selector: "PickupLocation > Code"
+      attribute :pick_location_type, selector: "PickupLocation", attr: "xsi:type"
+      # attribute :pick_location, selector: "PickupLocation",
+      #           parser: HotelBeds::Parser::PickupLocation
+      attribute :dest_location_code, selector: "DestinationLocation > Code"
+      attribute :dest_location_type, selector: "DestinationLocation", attr: "xsi:type"
+
+      attribute :duration, selector: "EstimatedTransferDuration"
 
       attribute :vehicle_type, selector: "ProductSpecifications > MasterVehicleType", attr: "name"
       attribute :product_type, selector: "ProductSpecifications > MasterProductType", attr: "name"
       attribute :service_type, selector: "ProductSpecifications > MasterServiceType", attr: "name"
-
-      attribute :all, selector: 'TransferInfo'
-      attribute :code, selector: "TransferInfo > Code"
-      attribute :type, selector: "TransferInfo > Type", attr: "code"
-      attribute :description, selector: "TransferInfo > DescriptionList > Description", multiple: true
-      attribute :images, selector: "TransferInfo > ImageList > Image > Url", multiple: true
-
       attribute :product_specification,
                 selector: "ProductSpecifications > TransferGeneralInfoList > TransferBulletPoint > Description",
                 multiple: true
